267ef0b783afcef3b44a6272470e3e0c827539a8
subbusudharsan <56117678+subbusudharsan@users.noreply.github.com>
2020-01-16 10:45:21 -0500

    Create FactorialNumber.java

---
+++ FactorialNumber.java
@@ -0,0 +1,14 @@
+
+public class FactorialNumber {
+public static void main(String[] args){
+int i;
+		int fact=1;
+
+		for(i=num;i>0;i--)
+		{
+		fact=fact*i;
+
+		}
+		System.out.println("Factorial of " +num+ " is " +fact);
+		}
+    }
